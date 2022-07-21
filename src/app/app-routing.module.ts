ad4865691213e63b55c4e61a2d0e796709f361f1
Grace Nyutu <graceynyutu@gmail.com>
2022-07-21 13:58:07 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'about', component: AboutComponent
+  },
+  {
+    path:'contact', component: ContactComponent
+  },
+  {
+    path:'projects', component: ProjectsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
